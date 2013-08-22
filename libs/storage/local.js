3632326703c22d048b07b9fe3cd1d63d868ef69f
Dongsu Jang <iolothebard@gmail.com>
2013-08-23 03:12:18 +0900

    replace my fileutils.js into q-io/fs. rewrite local storage provider for new interface.

--- libs/storage/local.js
+++ libs/storage/local.js
@@ -2,7 +2,9 @@
 
 var
   util = require('util'),
-  fileutils = require('../fileutils'),
+  path = require('path'),
+  Q = require('q'),
+  FS = require('q-io/fs'),
   StorageProvider = require('./storage').StorageProvider,
   debug = require('debug')('pictor:storage:local'),
   DEBUG = debug.enabled;
@@ -10,33 +12,58 @@ var
 /**
  * local file system based implementation of {@link StorageProvider}
  *
- * @param config
+ * `config` contains:
+ *
+ *    - {string} baseDir: base directory to store files
+ *    - {string} [baseUrl]: http url to access `baseDir`.
+ *
+ * NOTE: `config.baseUrl` should be `null` when this storage doesn't support http access.
+ *
+ * @param {object} config
  * @constructor
  */
 function LocalStorageProvider(config) {
   LocalStorageProvider.super_.apply(this, arguments);
   DEBUG && debug('create local storage provider: ', config);
+  FS.makeTree(this.config.baseDir).done();
 }
 util.inherits(LocalStorageProvider, StorageProvider);
 
-LocalStorageProvider.prototype.exists = function (storagePath) {
-  DEBUG && debug('local.exists', storagePath);
-  return fileutils.exists(storagePath);
+LocalStorageProvider.prototype.putFile = function (id, src) {
+  DEBUG && debug('local.putFile', src, '--->', id);
+  var dst = this._getPath(id);
+  var url = this._getUrl(id);
+  return FS.makeTree(path.dirname(dst))
+    .then(function () {
+      return FS.copy(src, dst);
+    })
+    .then(function () {
+      return {url: url, file: dst};
+    });
 };
 
-LocalStorageProvider.prototype.putFile = function (filePath, storagePath) {
-  DEBUG && debug('local.putFile', filePath, '---->', storagePath);
-  return fileutils.copyFile(filePath, storagePath);
+LocalStorageProvider.prototype.getFile = function (id) {
+  DEBUG && debug('local.getFile', id);
+  // NOTE: no copy!
+  var src = this._getPath(id);
+  var url = this._getUrl(id);
+  return FS.isFile(src)
+    .then(function (result) {
+      if (!result) {
+        // not exist or directory...
+        throw new Error('file not found');
+      }
+      return {url: url, file: src};
+    });
 };
 
-LocalStorageProvider.prototype.getFile = function (filePath, storagePath) {
-  DEBUG && debug('local.getFile', filePath, '<----', storagePath);
-  return fileutils.copyFile(storagePath, filePath);
-};
-
-LocalStorageProvider.prototype.deleteFile = function (storagePath) {
-  DEBUG && debug('local.deleteFile', storagePath);
-  return fileutils.deleteFile(storagePath);
+LocalStorageProvider.prototype.deleteFile = function (id) {
+  DEBUG && debug('local.deleteFile', id);
+  var src = this._getPath(id);
+  return FS.removeTree(src)
+    .then(function () {
+      return true;
+    });
 };
 
 module.exports = {
