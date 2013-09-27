d7257fda625c5c6db2ea9ab0bd747475bcfa7c23
Dongsu Jang <iolothebard@gmail.com>
2013-09-28 04:03:35 +0900

    add build scripts for client webapp in apps.

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,15 +12,71 @@ module.exports = function (grunt) {
         globals: {
         }
       },
-      files: ['libs/**/*.js']
+      libs: ['libs/**/*.js'],
+      routes: ['routes/**/*.js'],
+      app: ['app/js/**/*.js']
     },
     nodeunit: {
-      files: ['tests/**/*_test.js']
+      all: ['tests/**/*_test.js']
+    },
+    qunit: {
+      all: ['tests/**/*_test.html']
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      app: {
+        src: ['app/js/**/*.js'],
+        dest: 'build/app/js/all.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%=pkg.name%> <%=pkg.version%> (build at ' + (new Date()) + ') */\n'
+      },
+      app: {
+        files: {
+          'build/app/js/all.min.js': ['<%=concat.app.dest%>']
+        }
+      }
+    },
+    jade: {
+      options: {
+        pretty: false, // https://github.com/visionmedia/jade/issues/889
+        compileDebug: true
+      },
+      app: {
+        expand: true,
+        cwd: "app/",
+        src: ["**/*.jade"],
+        dest: "build/app",
+        ext: ".html"
+      }
+    },
+    copy: {
+      app: {
+        expand: true,
+        cwd: 'app/',
+        src: ['**', '!**/*.jade'],
+        dest: 'build/app'
+      }
+    },
+    watch: {
+      options: {nospawn: true},
+      app: {
+        files: ['app/**/*', '!app/**/*.jade', '!app/lib'],
+        tasks: ['copy']
+      },
+      jade: {
+        files: ['app/**/*.jade', '!app/lib'],
+        tasks: ['jade']
+      }
     },
     apidoc: {
-      files: {
-        src: 'libs/',
-        dest: 'public/docs/api'
+      routes: {
+        src: 'routes/',
+        dest: 'build/app/docs/api'
       }
     },
     clean: {
@@ -29,9 +85,13 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-apidoc');
 
   grunt.registerTask('rsync', function() {
@@ -52,7 +112,7 @@ module.exports = function (grunt) {
   grunt.registerTask('doxx', function() {
     var done = this.async();
     // XXX: doxx 0.6.0 contains broken template.
-    var doxx_cmdline = './node_modules/.bin/doxx --template ./scripts/doxx.jade --source libs --target public/docs/dox';
+    var doxx_cmdline = './node_modules/.bin/doxx --source libs --target build/app/docs/dox';
     require('child_process').exec(doxx_cmdline, function(err, stdout, stderr) {
       if(err) {
         grunt.verbose.writeln(stderr);
@@ -64,9 +124,27 @@ module.exports = function (grunt) {
       done(err);
     });
   });
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('node', function () {
+    grunt.util.spawn({
+      cmd: 'node',
+      args: ['app.js']
+    });
+  });
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('test', ['jshint', 'nodeunit', 'qunit']);
   grunt.registerTask('docs', ['doxx', 'apidoc']);
-  grunt.registerTask('build', ['clean', 'jshint', 'test', 'docs']);
-  grunt.registerTask('deploy', ['build', 'rsync']);
+  grunt.registerTask('build', ['jshint', 'concat', 'uglify', 'jade', 'copy']);
+  grunt.registerTask('deploy', ['build', 'docs', 'rsync']);
+  grunt.registerTask('run', ['build', 'node', 'watch']);
+
+  grunt.event.on('watch', function (action, filepath, target) {
+    if (grunt.file.isMatch(grunt.config('watch.app.files'), filepath)) {
+      var copySrc = filepath.replace(grunt.config('copy.app.cwd'), '');
+      grunt.config('copy.app.src', [copySrc]);
+    }
+    if (grunt.file.isMatch(grunt.config('watch.jade.files'), filepath)) {
+      var jadeSrc = filepath.replace(grunt.config('jade.app.cwd'), '');
+      grunt.config('jade.app.src', [jadeSrc]);
+    }
+  });
 };
