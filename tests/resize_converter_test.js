f6af8f82daf325e5acef8bc4243b5b68c2fc51d3
Dongsu Jang <iolothebard@gmail.com>
2013-10-16 01:52:39 +0900

    replace obsolete imgutils_test.js to separated test cases in convert/resize/crop_converter_test.js. XXX: NOT FULLY TESTED!

---
+++ tests/resize_converter_test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var
+  fs = require('fs'),
+  gm = require('gm'),
+  Q = require('q'),
+  Converter = require('../libs/converter/resize'),
+  src_jpg = __dirname + '/test.jpg',
+  src_png = __dirname + '/test.png',
+  src_gif = __dirname + '/test.gif',
+  dst_jpg = '/tmp/pictor_imgutils_test_dst.jpg',
+  dst_png = '/tmp/pictor_imgutils_test_dst.png',
+  dst_gif = '/tmp/pictor_imgutils_test_dst.gif';
+
+module.exports = {
+  setUp: function (callback) {
+    fs.unlink(dst_jpg, function () {
+      fs.unlink(dst_png, function () {
+        fs.unlink(dst_gif, function () {
+          callback();
+        });
+      });
+    });
+  },
+  test_convert: function (test) {
+    var converter = new Converter({});
+    var opts = {src: src_jpg, dst: dst_png, w: 123, h: 456, flags: '!'};
+    converter.convert(opts)
+      .then(function (result) {
+        console.log('convert ok:', result);
+        test.ok(result);
+        return Q.ninvoke(gm(dst_png), 'identify');
+      })
+      .then(function (result) {
+        console.log('convert->identify ok:', result);
+        test.equals(result.format, 'PNG');
+        test.equals(result.size.width, 123);
+        test.equals(result.size.height, 456);
+      })
+      .fail(function (err) {
+        console.log('convert err:', err);
+        test.ifError(err);
+      })
+      .done(test.done);
+  }
+}
