e28f2bcae53407cf07941884cae58f4afaa93a80
Dongsu Jang <iolothebard@gmail.com>
2013-10-17 00:31:14 +0900

    add sample presets using retina prefix '@2x'

--- libs/pictor.js
+++ libs/pictor.js
@@ -21,7 +21,14 @@ var
     m: {converter: 'thumbnail', w: 48},
     l: {converter: 'thumbnail', w: 64},
     xl: {converter: 'thumbnail', w: 128},
-    xxl: {converter: 'thumbnail', w: 256}
+    xxl: {converter: 'thumbnail', w: 256},
+    'xxs@2x': {converter: 'thumbnail', w: 32},
+    'xs@2x': {converter: 'thumbnail', w: 48},
+    's@2x': {converter: 'thumbnail', w: 64},
+    'm@2x': {converter: 'thumbnail', w: 96},
+    'l@2x': {converter: 'thumbnail', w: 128},
+    'xl@2x': {converter: 'thumbnail', w: 256},
+    'xxl@2x': {converter: 'thumbnail', w: 512}
   },
   converters = {},
   dataStorage,
