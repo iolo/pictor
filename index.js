755a10c1a4b6e1161349d0236c0d969f654dc714
Dongsu Jang <iolothebard@gmail.com>
2013-10-28 03:01:35 +0900

    export routes, config and app module.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 module.exports = require('./libs/pictor');
-module.exports.routes = require('./routes');
+
+module.exports.__defineGetter__('routes', function () {
+  return require('./routes');
+});
+module.exports.__defineGetter__('config', function () {
+  return require('./config');
+});
+module.exports.__defineGetter__('app', function () {
+  return require('./app');
+});
